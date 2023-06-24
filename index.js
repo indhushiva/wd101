9c966b1a28359157686572db34c8730cb6aa4c2f
indhushiva <137480127+indhushiva@users.noreply.github.com>
2023-06-24 12:54:57 +0530

    index.js

--- index.js
+++ index.js
@@ -1,128 +1,24 @@
-function GEEKFORGEEKS(){
-	var name =
-		document.forms.RegForm.Name.value;
-	var email =
-		document.forms.RegForm.EMail.value;
-	var phone =
-		document.forms.RegForm.Telephone.value;
-	var what =
-		document.forms.RegForm.Subject.value;
-	var password =
-		document.forms.RegForm.Password.value;
-	var address =
-		document.forms.RegForm.Address.value;
-	//Javascript reGex for Email Validation.
-	var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
-	// Javascript reGex for Phone Number validation.
-	var regPhone=/^\d{10}$/;					
-	// Javascript reGex for Name validation
-	var regName = /\d+$/g;						
-
-	if (name == "" || regName.test(name)) {
-		window.alert("Please enter your name properly.");
-		name.focus();
-		return false;
-	}
-
-	if (address == "") {
-		window.alert("Please enter your address.");
-		address.focus();
-		return false;
-	}
+<html>
+<head>
+<title>Registration Form</title>
+</head>
+<body>
+<div id="data">
+	<form align="center"><h3><b>Student Form</b></h3><br>
+Name : <input type="text" id="Name"><br><br>
+Email : <input type="text" id="Email"><br><br>
+Password : <input type="text" id="Password"><br><br>
+Date of Birth : <input type="date" id="DOB"><br><br>
+	<input type="checkbox" id= "check">
 	
-	if (email == "" || !regEmail.test(email)) {
-		window.alert("Please enter a valid e-mail address.");
-		email.focus();
-		return false;
-	}
-	
-	if (password == "") {
-		alert("Please enter your password");
-		password.focus();
-		return false;
-	}
+<button onclick="AddData()">Add</button>
+</form>
+</div>
 
-	if(password.length <6){
-		alert("Password should be atleast 6 character long");
-		password.focus();
-		return false;
-
-	}
-	if (phone == "" || !regPhone.test(phone)) {
-		alert("Please enter valid phone number.");
-		phone.focus();
-		return false;
-	}
-
-	if (what.selectedIndex == -1) {
-		alert("Please enter your course.");
-		what.focus();
-		return false;
-	}
-
-	return true;
-} {
-	var name =
-		document.forms.RegForm.Name.value;
-	var email =
-		document.forms.RegForm.EMail.value;
-	var phone =
-		document.forms.RegForm.Telephone.value;
-	var what =
-		document.forms.RegForm.Subject.value;
-	var password =
-		document.forms.RegForm.Password.value;
-	var address =
-		document.forms.RegForm.Address.value;
-	//Javascript reGex for Email Validation.
-	var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
-	// Javascript reGex for Phone Number validation.
-	var regPhone=/^\d{10}$/;									
-	// Javascript reGex for Name validation
-	var regName = /\d+$/g;								
-	
-
-	if (name == "" || regName.test(name)) {
-		window.alert("Please enter your name properly.");
-		name.focus();
-		return false;
-	}
-
-	if (address == "") {
-		window.alert("Please enter your address.");
-		address.focus();
-		return false;
-	}
-	
-	if (email == "" || !regEmail.test(email)) {
-		window.alert("Please enter a valid e-mail address.");
-		email.focus();
-		return false;
-	}
-	
-	if (password == "") {
-		alert("Please enter your password");
-		password.focus();
-		return false;
-	}
-
-	if(password.length <6){
-		alert("Password should be atleast 6 character long");
-		password.focus();
-		return false;
-
-	}
-	if (phone == "" || !regPhone.test(phone)) {
-		alert("Please enter valid phone number.");
-		phone.focus();
-		return false;
-	}
-
-	if (what.selectedIndex == -1) {
-		alert("Please enter your course.");
-		what.focus();
-		return false;
-	}
-
-	return true;
-}
+<div id="tab">
+<table id="list" cellspacing="3" cellpadding="3" border="1"><thead>
+<tr>
+<td>Name</td><td>Branch</td><td>Address</td><td>Contact</td>
+</tr></thead>
+<tbody></tbody></table>
+</div>
