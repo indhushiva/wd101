c41d130c8e3fb9ef653ae76683cdfeac4fb6fd04
indhushiva <137480127+indhushiva@users.noreply.github.com>
2023-06-26 11:28:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,44 @@
 </head>
 <body>
 <div id="data">
-	<form align="center"><h3><b>Student Form</b></h3><br>
-Name : <input type="text" id="Name"><br><br>
-Email : <input type="text" id="Email"><br><br>
-Password : <input type="text" id="Password"><br><br>
-Date of Birth : <input type="date" id="DOB"><br><br>
-	<input type="checkbox" id= "check">
+<form align="center"><h3><b>Student Form</b></h3><br>
+<label for="name"><b>Name</b></label>
+<input type="text" id="name" required><br><br>
+<label for="email"><b>Email</b></label>
+<input type="text" id="email" required><br><br>
+<label for="name"><b>Password</b></label>
+<input type="text" id="password" required><br><br>
+<label for="name"><b>Date of Birth</b></label>
+<input type="date" id="DOB" required><br><br>
+<input type="checkbox" id="accept" name="accept" value="yes">  
+<label for="accept"> Accept Terms and Conditions </label>
 	
 <button onclick="AddData()">Add</button>
 </form>
 </div>
 
 <div id="tab">
+<h1> Entries </h1>
 <table id="list" cellspacing="3" cellpadding="3" border="1"><thead>
 <tr>
-<td>Name</td><td>Branch</td><td>Address</td><td>Contact</td>
+<td>Name</td><td>Email</td><td>Password</td><td>Date of Birth</td>
 </tr></thead>
 <tbody></tbody></table>
 </div>
+
+<script>
+function AddData(e)
+{
+e.preventDefault();
+var rows="";
+var name=document.getElementById("Name").value;
+var email=document.getElementById("Email").value;
+var password=document.getElementById("Password").value;
+var DOB=document.getElementById("DOB").value;
+rows+="<tr><td>"+name+"</td><td>"+email+"</td><td>"+password+"</td><td>"+DOB+"</td></tr>";
+$(rows).appendTo("#list tbody");
+}
+</script>
+
+</body>
+</html>
